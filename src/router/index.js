6b930f011f2b7dc7494297e7bb7e2c9a71a1e2e3
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-09-07 16:49:30 +0800

    modify: delete cityview in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import CityView from '../views/CityView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,14 +10,6 @@ const router = createRouter({
       component: HomeView,
       meta: {
         title: 'home'
-      }
-    },
-    {
-      path: '/weather/:city/:state',
-      name: 'city',
-      component: CityView,
-      meta: {
-        title: 'Weather'
       }
     }
   ]
