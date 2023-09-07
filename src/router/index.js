ed409e6959fbff024ae245c9cc27e20a6a0fe2b9
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-09-07 16:11:37 +0800

    modify: recreate cityview file

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import CityView from '../views/CityView.vue'
 import HomeView from '../views/HomeView.vue'
+import CityView from '../views/CityView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
